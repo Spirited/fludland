8c6f15e8d5964220a6eb9b0fefa976bc7121df0c
Spirited <sergey_kompanets@ukr.net>
2024-09-17 20:27:33 +0300

    user DAO test coverage

--- sso/src/test/java/org/fludland/sso/repository/UserRepositoryTest.java
+++ sso/src/test/java/org/fludland/sso/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.fludland.sso.repository;
 
+import org.fludland.sso.entities.Profile;
 import org.fludland.sso.entities.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,58 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-@SqlGroup(@Sql({"classpath:/data.sql"}))
+@SqlGroup(@Sql({"classpath:/cleanup.sql", "classpath:/data.sql"}))
 class UserRepositoryTest extends AbstractIntegrationTest {
     @Autowired
     UserRepository userRepository;
 
     @Test
-    void test() {
+    void test_when_execute_findAll_then_return_2_users() {
         List<User> all = userRepository.findAll();
         assertThat(all).isNotNull();
         assertThat(all).hasSize(2);
         System.out.println("Fetched users: " + all);
+    }
+
+    @Test
+    void test_when_save_new_user_then_return_3_members() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("password");
+        userRepository.save(user);
+
+        List<User> all = userRepository.findAll();
+        assertThat(all).isNotNull();
+        assertThat(all).hasSize(3);
+    }
+
+    @Test
+    void test_select_user_by_id_then_return_not_null_user() {
+        User user = userRepository.findById(1L).orElse(null);
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    void test_just_saved_user_fetch_id_by_id_then_return_user_fields() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("password");
+        userRepository.save(user);
+        User savedUser = userRepository.findById(3L).orElse(null);
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getUsername()).isEqualTo("test");
+        assertThat(savedUser.getPassword()).isEqualTo("password");
+
+    }
+
+    @Test
+    void test_save_users_profile_then_return_not_null_profile() {
+        Profile profile = new Profile();
+
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("password");
+        user.set
+        userRepository.save(user);
     }
 }
--- sso/src/test/resources/cleanup.sql
+++ sso/src/test/resources/cleanup.sql
@@ -1,8 +1,8 @@
 begin transaction;
 
-truncate table users;
-truncate table profiles;
-truncate table token;
+truncate table sso.profiles CASCADE ;
+-- truncate table sso.token;
+truncate table sso.users CASCADE ;
 
 alter sequence sso.users_id_seq restart with 1;
 alter sequence sso.profiles_id_seq restart with 1;
