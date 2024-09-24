f922e1f1ae36b50c60cca5f59ac291eabd879c48
Spirited <sergey_kompanets@ukr.net>
2024-09-24 22:03:29 +0300

    implemented TokenUtils - generate and validate token methods

--- sso/src/main/java/org/fludland/sso/utils/TokenUtils.java
+++ sso/src/main/java/org/fludland/sso/utils/TokenUtils.java
@@ -1,9 +1,41 @@
 package org.fludland.sso.utils;
 
-public final class TokenUtils {
-    private TokenUtils() {}
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.stereotype.Component;
 
-    public static String generateToken() {
-        return "";
+import java.util.Date;
+
+/**
+ * Provides token utils like create, validate, etc.
+ * Used example from <a href=https://medium.com/@salvipriya97/jwt-implementation-example-f36061be1405>medium article</a>
+ */
+@Component
+public final class TokenUtils {
+
+    private static final String SECRET = "mySecretKey";  // Secret key for signing JWT
+    private static final long EXPIRATION_TIME = 86400000;
+
+    public String generateJWT(String username) {
+        return JWT.create()
+                .withSubject(username)  // Username or user ID
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))  // Expiration time
+                .sign(Algorithm.HMAC256(SECRET));
+    }
+
+    // Method to validate the JWT token
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);  // If token is valid, decode it
+            return decodedJWT.getSubject();  // Return the username from token
+        } catch (JWTVerificationException e) {
+            return null;  // Invalid token
+        }
     }
 }
--- sso/src/test/java/org/fludland/sso/service/AuthorizationServiceTest.java
+++ sso/src/test/java/org/fludland/sso/service/AuthorizationServiceTest.java
@@ -7,6 +7,7 @@ import org.fludland.sso.exceptions.UsernameAlreadyExistsException;
 import org.fludland.sso.repository.ProfileRepository;
 import org.fludland.sso.repository.UserRepository;
 import org.fludland.sso.service.impl.AuthorizationServiceImpl;
+import org.fludland.sso.utils.TokenUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,6 +25,7 @@ import static org.mockito.Mockito.when;
 class AuthorizationServiceTest {
     private UserRepository moockUserRepository;
     private ProfileRepository mockProfileRepository;
+    private TokenUtils mockTokenUtils;
 
     private AuthorizationService authorizationService;
 
@@ -30,12 +33,16 @@ class AuthorizationServiceTest {
     public void init() {
         moockUserRepository = mock(UserRepository.class);
         mockProfileRepository = mock(ProfileRepository.class);
-        authorizationService = new AuthorizationServiceImpl(moockUserRepository, mockProfileRepository);
+        mockTokenUtils = mock(TokenUtils.class);
+        authorizationService = new AuthorizationServiceImpl(moockUserRepository, mockProfileRepository, mockTokenUtils);
     }
 
     @Test
     void test_when_passed_user_auth_data_expected_new_user() {
         LoginCreateDto loginCreateDto = new LoginCreateDto("foo", "bar");
+
+
+        when(mockTokenUtils.generateJWT(anyString())).thenReturn("12345");
 
         SuccessfulRegistration register = authorizationService.register(loginCreateDto);
         assertThat(register.getToken()).isNotNull().isEqualTo("12345");
