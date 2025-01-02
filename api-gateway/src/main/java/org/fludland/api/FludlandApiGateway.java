030fdbe0418e7b5d5924f3b4c9dceeca3f1fd4a6
Spirited <sergey_kompanets@ukr.net>
2025-01-02 21:58:22 +0200

    inited api-gateway service

---
+++ api-gateway/src/main/java/org/fludland/api/FludlandApiGateway.java
@@ -0,0 +1,11 @@
+package org.fludland.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FludlandApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(FludlandApiGateway.class, args);
+    }
+}
---
+++ api-gateway/src/main/java/org/fludland/api/config/UserServiceClient.java
@@ -0,0 +1,7 @@
+package org.fludland.api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserServiceClient {
+}
