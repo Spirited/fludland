948194c81f9a4539bcac5200f50a207e74bbd483
Spirited <sergey_kompanets@ukr.net>
2025-01-06 16:15:43 +0200

    login page fixed issues

--- fludland-portal/src/components/LoginPage.jsx
+++ fludland-portal/src/components/LoginPage.jsx
@@ -45,3 +45,5 @@ const LoginPage = () => {
     );
 };
 
+export default LoginPage
+
--- fludland-portal/src/route.jsx
+++ fludland-portal/src/route.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import LoginPage from './components/LoginPage.jsx'
-// import './index.css'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <LoginPage />
     </StrictMode>,
 )
-
-export default LoginPage
