230a03332fcc59aec075d58e4537f92903a78339
Spirited <sergey_kompanets@ukr.net>
2025-01-22 22:30:32 +0200

    main page route

--- fludland-portal/src/components/RouteConfigs.jsx
+++ fludland-portal/src/components/RouteConfigs.jsx
@@ -2,12 +2,14 @@ import {Routes, Route} from "react-router-dom";
 import LoginPage from "./LoginPage.jsx";
 import RegistrationForm from "./RegistrationForm.jsx";
 import {WelcomePage} from "../pages/Pages.jsx";
+import MainPage from "../pages/MainPage.jsx";
 
 const RouteConfigs = () => (
     <Routes>
         <Route path="/" element={ <WelcomePage /> }/>
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/signup" element={<RegistrationForm />} />
+        <Route path="/main" element={<MainPage />} />
     </Routes>
 );
 
---
+++ fludland-portal/src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+const MainPage = () => {
+    return (
+        <div className="container">
+            <h2>Welcome to the main page</h2>
+            <p>This is main user page!!!!!</p>
+        </div>
+    );
+};
+
+export default MainPage;
