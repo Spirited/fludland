19c204bf5699ec5da8c33607831dd1d48d3e991a
Spirited <sergey_kompanets@ukr.net>
2025-02-12 23:09:02 +0200

    [user-service] added ErrorControllerAdvice

---
+++ user-sevice/src/main/java/org/fludland/userservice/controller/ErrorControllerAdvice.java
@@ -0,0 +1,24 @@
+package org.fludland.userservice.controller;
+
+import org.fludland.common.ErrorCodes;
+import org.fludland.common.ErrorResponse;
+import org.fludland.userservice.exceptions.ProfileNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorControllerAdvice {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorControllerAdvice.class);
+
+    @ExceptionHandler(ProfileNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(ProfileNotFoundException ex) { // 404
+        LOGGER.warn(ex.getMessage(), ex);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ErrorCodes.PROFILE_NOT_FOUND));
+    }
+}
