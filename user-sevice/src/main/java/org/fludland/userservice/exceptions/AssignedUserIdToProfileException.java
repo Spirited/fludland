530dbfdd3ad537ad439e41b1643d3059c0b6b91d
Spirited <sergey_kompanets@ukr.net>
2025-02-15 21:12:40 +0200

    [user-service] added AssignedUserIdToProfileException

---
+++ user-sevice/src/main/java/org/fludland/userservice/exceptions/AssignedUserIdToProfileException.java
@@ -0,0 +1,10 @@
+package org.fludland.userservice.exceptions;
+
+public class AssignedUserIdToProfileException extends ApplicationException {
+    public AssignedUserIdToProfileException() {
+    }
+
+    public AssignedUserIdToProfileException(String message) {
+        super(message);
+    }
+}
