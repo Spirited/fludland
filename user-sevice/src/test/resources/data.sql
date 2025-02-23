9b52d21afdeedd2b4506c38daad7f6dedffd0c1e
Spirited <sergey_kompanets@ukr.net>
2025-02-23 19:45:49 +0200

    [user-service] added check method is user B already following user A. (2)

--- user-sevice/src/test/resources/data.sql
+++ user-sevice/src/test/resources/data.sql
@@ -1,6 +1,6 @@
 set search_path to userservice;
 
-insert into userprofile(firstname, lastname, birthday, gender, email, phone, userid, logoid)
+insert into userprofile(firstname, lastname, birthday, gender, email, phone, user_id, logoid)
 values ('john', 'smith', '12-11-1987', 'MALE', 'john@smith.com', '380971228367', 42, 13);
 
 commit;
