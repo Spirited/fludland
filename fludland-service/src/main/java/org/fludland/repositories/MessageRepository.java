5ff02f96719e41a0f4466bde74058c52bbbc2102
Spirited <sergey_kompanets@ukr.net>
2025-03-02 22:46:24 +0200

    [fludland-service] added messages repository

---
+++ fludland-service/src/main/java/org/fludland/repositories/MessageRepository.java
@@ -0,0 +1,9 @@
+package org.fludland.repositories;
+
+import org.fludland.entities.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
