ee9d054a443e44deae0a8684b116b5d60a577a74
Spirited <sergey_kompanets@ukr.net>
2025-03-10 22:17:40 +0200

    [sso] updated token table

--- sso/src/main/resources/db/sql/token.sql
+++ sso/src/main/resources/db/sql/token.sql
@@ -1,8 +1,11 @@
 create table token (
-    id UUID NOT NULL,
+    id BIGSERIAL NOT NULL,
     token varchar(255) NOT NULL,
     refresh_token varchar(255),
+    user_id BIGINT NOT NULL,
     created_at timestamp NOT NULL,
     expired_at timestamp NOT NULL,
-    CONSTRAINT pk_token PRIMARY KEY (id)
+
+    CONSTRAINT pk_token PRIMARY KEY (id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
