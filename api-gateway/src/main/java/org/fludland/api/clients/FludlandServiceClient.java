833a95b05ca81f8b769a376ee4044e712dc382a2
Spirited <sergey_kompanets@ukr.net>
2025-03-16 23:01:52 +0200

    [portal] added fludland service client

--- api-gateway/src/main/java/org/fludland/api/clients/FludlandServiceClient.java
+++ api-gateway/src/main/java/org/fludland/api/clients/FludlandServiceClient.java
@@ -1,7 +1,31 @@
 package org.fludland.api.clients;
 
-import org.springframework.context.annotation.Configuration;
+import org.fludland.common.ErrorResponse;
+import org.fludland.service.CreatePostDto;
+import org.fludland.service.EditPostDto;
+import org.fludland.service.PostDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
 
-@Configuration
-public class FludlandServiceClient {
+import java.util.List;
+
+@FeignClient(value = "fludland-service", url = "${api.fludland.host}")
+public interface FludlandServiceClient {
+    @PostMapping("/posts")
+    PostDto createPost(@RequestBody CreatePostDto createPostDto);
+
+    @GetMapping("/posts/{id}")
+    PostDto getPostById(@PathVariable String id);
+
+    @GetMapping("/posts/users/{userId}")
+    List<PostDto> getPostByUserId(@PathVariable String userId);
+
+    @GetMapping("/posts")
+    List<PostDto> getAllPosts();
+
+    @PutMapping("/posts/{id}")
+    PostDto editPost(@PathVariable String id, @RequestBody EditPostDto editPostDto);
+
+    @DeleteMapping("/posts/{id}")
+    ErrorResponse deletePost(@PathVariable String id);
 }
