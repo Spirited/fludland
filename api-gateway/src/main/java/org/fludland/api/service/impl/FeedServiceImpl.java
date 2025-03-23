c6e4ce6148c49f8dd86d78d1921d7133f1328bc3
Spirited <sergey_kompanets@ukr.net>
2025-03-23 20:18:27 +0200

    [portal] feed draft 2

--- api-gateway/src/main/java/org/fludland/api/service/impl/FeedServiceImpl.java
+++ api-gateway/src/main/java/org/fludland/api/service/impl/FeedServiceImpl.java
@@ -1,11 +1,22 @@
 package org.fludland.api.service.impl;
 
+import org.fludland.api.clients.FludlandServiceClient;
 import org.fludland.api.service.FeedService;
 import org.fludland.service.PostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FeedServiceImpl implements FeedService {
+    private final FludlandServiceClient fludlandServiceClient;
+
+    @Autowired
+    public FeedServiceImpl(final FludlandServiceClient fludlandServiceClient) {
+        this.fludlandServiceClient = fludlandServiceClient;
+    }
+
     @Override
     public List<PostDto> myFeed() {
         //TODO
