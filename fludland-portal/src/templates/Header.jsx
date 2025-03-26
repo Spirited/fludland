f1aea197589d9999b8988d78dc39f085ae801478
Spirited <sergey_kompanets@ukr.net>
2025-03-26 20:54:50 +0200

    [portal] added logo and icon

--- fludland-portal/src/templates/Header.jsx
+++ fludland-portal/src/templates/Header.jsx
@@ -1,9 +1,10 @@
-// import "../css/header_and_footer.css"
 import {Link} from "react-router-dom";
 
 const Header = () => (
     <header className="header">
-        <div className="logo">MyLogo</div>
+        <div className="logo">
+            <a href="/"><img src="../../public/images/logo.png" height="100" width="100"/></a>
+        </div>
         <h1>My App Header</h1>
         <div className="actions">
             <nav>
