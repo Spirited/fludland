6bec259603500a02b43c35edc54acae993b862ca
Spirited <sergey_kompanets@ukr.net>
2025-03-28 19:48:36 +0200

    [common] added the base64 utils 2

--- common/src/main/java/org/fludland/utils/Base64Utils.java
+++ common/src/main/java/org/fludland/utils/Base64Utils.java
@@ -1,5 +1,6 @@
 package org.fludland.utils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
@@ -7,8 +8,14 @@ import java.util.Base64;
 public final class Base64Utils {
     private Base64Utils() {}
 
-    public static String fileToBase64(InputStream file) throws IOException{
+    public static String fileToBase64(final InputStream file) throws IOException{
         byte[] encoded = Base64.getEncoder().encode(file.readAllBytes());
         return new String(encoded);
+    }
+
+    public static InputStream base64ToFile(final String base64) {
+        byte[] decode = Base64.getDecoder().decode(base64);
+
+        return new ByteArrayInputStream(decode);
     }
 }
